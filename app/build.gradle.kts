637815f19a3dbb9757f3b57c667ea75423e49997
Francesco Canossi <canossifrancesco4@protonmail.com>
2026-05-02 10:49:26 +0200

    V1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.lazycal"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0 Beta"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
